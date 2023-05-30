753381802fd0a5fba83e026c2fef898b8cc0af33
Ulises <ulises.aviles@cetys.edu.mx>
2023-05-30 01:34:42 -0700

    Format and onChange type

--- app/components/input/input.tsx
+++ app/components/input/input.tsx
@@ -1,39 +1,48 @@
-import { HTMLInputTypeAttribute, useEffect, useState } from 'react';
-import styles from './styles.module.scss';
+import { HTMLInputTypeAttribute, useEffect, useState } from "react";
+import styles from "./styles.module.scss";
 
 interface InputProps {
   label?: string;
   defaultValue?: string;
-  onChange?: any;
+  onChange?: (value: string) => void;
   placeholder?: string;
   type?: HTMLInputTypeAttribute;
   disabled?: boolean;
-  required?: boolean
+  required?: boolean;
 }
 
-export const useInput = ({label, defaultValue = '', onChange, placeholder, type = 'text', disabled, required}: InputProps): [string, JSX.Element] => {
+export const useInput = ({
+  label,
+  defaultValue = "",
+  onChange,
+  placeholder,
+  type = "text",
+  disabled,
+  required,
+}: InputProps): [string, JSX.Element] => {
   const [currentValue, setValue]: [string, any] = useState(defaultValue);
 
-  useEffect(() => {
-
-  }, [currentValue]);
+  useEffect(() => {}, [currentValue]);
 
   const handleChange = (ev: any) => {
     setValue(ev.target.value);
     if (onChange) {
       onChange(ev.target.value);
     }
-  }
+  };
 
-  const inputValue = <div className={styles.wrapper}>
-                      {label && <p>{`${label} ${required ? '*' : ''}`}</p>}
-                      <input
-                      className={`${styles.input} ${disabled ? styles.disabled : ''}`}
-                      defaultValue={defaultValue}
-                      placeholder={placeholder}
-                      type={type}
-                      onChange={handleChange}
-                      disabled={disabled}/>
-                    </div>;
+  const inputValue = (
+    <div className={styles.wrapper}>
+      {label && <p>{`${label} ${required ? "*" : ""}`}</p>}
+      <input
+        className={`${styles.input} ${disabled ? styles.disabled : ""}`}
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        type={type}
+        onChange={handleChange}
+        disabled={disabled}
+      />
+    </div>
+  );
   return [currentValue, inputValue];
-}
+};
